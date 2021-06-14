74d423c40740e6b9d72c87dffb127bf2a310ca10
t.zemmari <tarik.zemmari@xabec.es>
2021-06-14 14:11:40 +0200

    ya se puede hacer un login , ahora a por el token

--- src/app/Services/user.service..ts
+++ src/app/Services/user.service..ts
@@ -1,36 +1,48 @@
-import {Injectable} from '@angular/core';
-import {HttpClient ,HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {User} from '../../Models/User';
-import {global} from './global';
+import { User } from '../../Models/User';
+import { global } from './global';
 
 @Injectable()
 
 export class UserService {
 
-    public url:string;
+    public url: string;
 
     constructor(
         public _http: HttpClient
-    ){
+    ) {
         this.url = global.url;
     }
 
-    prueba(){
+    prueba() {
         return "Hola ya estamos aqui";
     }
-    register(user):Observable<any>{
+    register(user): Observable<any> {
 
         let json = JSON.stringify(user);
-        let params = 'json='+json;
+        let params = 'json=' + json;
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+        return this._http.post(this.url + "register", params, { headers: headers });
+    }
+
+    login(user,getToken = null):Observable<any>{
+
+        if(getToken !=null){
+
+            user.getToken = 'true';
+        }
+
+        let json = JSON.stringify(user);
+        let params = 'json=' + json;   
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
     
-        let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
-
-        return this._http.post(this.url+"register",params,{headers:headers});
-
-
-
-
-
-}
+        return this._http.post(this.url + "login", params, { headers: headers });
+    
+    }
 }
--- src/componentes/login/login.component.ts
+++ src/componentes/login/login.component.ts
@@ -1,19 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { User} from '../../Models/User';
+import {NgForm} from '@angular/forms';
+import { UserService} from '../../app/Services/user.service.';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers:[UserService]
 })
 export class LoginComponent implements OnInit {
 
 
   public page_title: string;
-  constructor() {
-    this.page_title = "Login"
+  public status :string;
+  public user :User;
+  constructor(
+    private _userService:UserService
+  ) {
+    this.page_title="Identificate";
+    this.user = new User(1,"","","","","ROLE_USER","");
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    this._userService.login(this.user).subscribe(
+
+      response => {
+
+      console.log(response)
+      if(!response.Status || response.Status != 'Error'){
+
+        this.status= 'Success';
+        this.user = response;
+        console.log(this.user)
+        form.reset();
+      }else{
+        this.status = 'error';
+      }
+
+      },
+      error => {
+
+        this.status = 'error';
+        console.log(error)
+      }
+    )
   }
 
 }
