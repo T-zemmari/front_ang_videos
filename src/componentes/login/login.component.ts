af9f6574d7e11d0128a3181fd51decc22dacb7a2
t.zemmari <tarik.zemmari@xabec.es>
2021-06-14 14:58:10 +0200

    ya se puede conseguir el token

--- src/componentes/login/login.component.ts
+++ src/componentes/login/login.component.ts
@@ -1,45 +1,91 @@
 import { Component, OnInit } from '@angular/core';
-import { User} from '../../Models/User';
-import {NgForm} from '@angular/forms';
-import { UserService} from '../../app/Services/user.service.';
+import { User } from '../../Models/User';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../app/Services/user.service.';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  providers:[UserService]
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
 
   public page_title: string;
-  public status :string;
-  public user :User;
+  public status: string;
+  public user: User;
+  public token: string;
+  public identity;
+
+
   constructor(
-    private _userService:UserService
+    private _userService: UserService
   ) {
-    this.page_title="Identificate";
-    this.user = new User(1,"","","","","ROLE_USER","");
+    this.page_title = "Identificate";
+    this.user = new User(1, "", "", "", "", "ROLE_USER", "");
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form){
+  onSubmit(form) {
+
+
     this._userService.login(this.user).subscribe(
 
       response => {
 
-      console.log(response)
-      if(!response.Status || response.Status != 'Error'){
 
-        this.status= 'Success';
-        this.user = response;
-        console.log(this.user)
-        form.reset();
-      }else{
-        this.status = 'error';
-      }
+
+
+        if (!response.Status || response.Status != 'Error') {
+
+
+          this.status = 'Success';
+          this.identity = response;
+
+
+
+
+          /////-------------devolver el token------------------------//
+
+
+          this._userService.login(this.user, true).subscribe(
+
+            response => {
+              if (!response.Status || response.Status != 'Error') {
+
+
+                this.status = 'Success';
+                this.token = response;
+
+                console.log(this.identity);
+                console.log(this.token);
+
+
+
+              } else {
+                this.status = 'error';
+              }
+
+            },
+            error => {
+
+              this.status = 'error';
+
+            }
+          );
+
+
+
+          ////////////////////////////////////////////////////////////
+
+
+
+        } else {
+          this.status = 'error';
+        }
 
       },
       error => {
@@ -47,7 +93,7 @@ export class LoginComponent implements OnInit {
         this.status = 'error';
         console.log(error)
       }
-    )
+    );
   }
 
 }
