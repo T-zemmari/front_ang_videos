1ea8c2ce923d870b86672b8cc6d3cc76963e1ab4
t.zemmari <tarik.zemmari@xabec.es>
2021-06-14 15:34:09 +0200

    Despues de conseguir el login , se persisten los datos mediante localstorage y se envocan en service_user para usarlos en cualquier vista o funcions js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { UserService } from './Services/user.service.';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'vang';
+
+  public identity;
+  public token;
+
+
+  constructor(
+    private _userService : UserService
+  ){
+
+  }
+
+
+
+  ngOnInit(){
+
+   this.loadService();
+
+
+
+  }
+
+
+  loadService(){
+
+    this.identity= this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 }
